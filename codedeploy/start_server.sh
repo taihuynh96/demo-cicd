c36e0ea53488ff818d16ad615f2923053e3cb850
kum_cp <kum_cp@yahoo.com.vn>
2022-04-05 08:23:16 +0700

    modify docker-compose

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -3,4 +3,4 @@
 
 # $(aws ecr get-login --no-include-email --region $REGION)
 
-# cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
+cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
