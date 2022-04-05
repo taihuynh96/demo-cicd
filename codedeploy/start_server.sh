8bf21d4b596d5533b6e1214d1277f11b0fd262ff
kum_cp <kum_cp@yahoo.com.vn>
2022-04-05 18:29:03 +0700

    update code

--- codedeploy/start_server.sh
+++ codedeploy/start_server.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+aws ecr get-login-password --region ap-southeast-1 | docker login --username AWS --password-stdin 666402361323.dkr.ecr.ap-southeast-1.amazonaws.com
+
 cd /home/ec2-user/web/ && /usr/local/bin/docker-compose -f docker-compose.yml up -d
